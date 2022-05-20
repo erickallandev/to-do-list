acb3b9660f29aab6a588c23a4a8172a0e777157d
Erick Allan Silva <98674896+erickallandev@users.noreply.github.com>
2022-05-20 16:28:44 -0300

    feat: build initial screen features with styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as C from './App.styled';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <C.Container>
+            <C.Area>
+                <C.Header>To do List</C.Header>
+
+                {/* Section to add new activities */}
+
+                {/* Section to show the to do list */}
+                
+            </C.Area>
+        </C.Container>
+    );
 }
 
 export default App;
